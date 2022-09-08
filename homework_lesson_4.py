f39242b00c8b879eb0c223b1918f75492d50607f
Oleg_Chikhalin <oleg.chikhalin@gmail.com>
2022-09-08 21:14:13 +0300

    added homework for lesson 4

---
+++ homework_lesson_4.py
@@ -0,0 +1,18 @@
+
+
+def open_browser(browser_name):
+    print(browser_name.__name__ + " " + str(browser_name))
+
+
+
+
+def go_to_companyname_homepage(page_url):
+    print(go_to_companyname_homepage.__name__ + " " + str(page_url))
+
+
+
+
+
+
+def find_registration_button_on_login_page(page_url, button_text):
+    print(find_registration_button_on_login_page.__name__ + " " + str(page_url) + " " + str(button_text))
